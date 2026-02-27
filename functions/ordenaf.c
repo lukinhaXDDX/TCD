64b9463c0b5f0640b42250cc3957702ca64440ab
Lucas Garcia Campos <lucaslukinha774@gmail.com>
2026-02-27 17:34:10 -0300

    criei o quick sort mas ainda falta colocar na main

--- functions/ordenaf.c
+++ functions/ordenaf.c
@@ -80,3 +80,41 @@ void selectionSort(int vetor[], int size){
     }
 
 }
+
+int particiona (int*v, int inicio, int fim)
+{
+    int pivo= (v[inicio] + v[fim] + v[(inicio + fim) / 2]) / 3;
+    while(inicio<fim)
+    {
+        while(inicio < fim && v[inicio] <= pivo)
+        {
+            inicio++;
+        }
+        while(inicio<fim && v[fim]>pivo)
+        {
+            fim--;
+        }
+        int aux= v[inicio];
+        v[inicio] =v[fim];
+        v[fim]= aux;
+    }
+    return inicio;
+}
+
+void quickSort(int *v, int inicio, int fim)
+{
+    if(inicio<fim)
+    {
+       int pos= particiona(v, inicio, fim);
+       quickSort(v,inicio , pos-1);
+       quickSort(v,pos,fim);
+    }    
+}
+void imprimir(int *v, int tam)
+{
+    for(int i=0 ; i<tam; i++)
+    {
+        printf("%d", v[i]);
+        printf("\n");
+    }
+}
